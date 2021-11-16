e34be41420e83da59d4df4b3c72492222b03a2bb
Vidar Magnusson <git@vidarmagnusson.com>
2021-11-17 00:29:55 +0100

    Finished day 2, 2020 in rust

--- 2020_rust/src/days/day_2/mod.rs
+++ 2020_rust/src/days/day_2/mod.rs
@@ -1,4 +1,4 @@
-use days::common::AOCInput;
+use crate::days::common::{AOCInput};
 
 struct Policy {
     min: u32,
@@ -6,23 +6,80 @@ struct Policy {
     letter: char,
 }
 
-struct Password {
+impl Policy {
+    fn parse(input: &str) -> Policy {
+        let parts = input.split(" ").collect::<Vec<&str>>();
+        let span = parts[0].split("-").collect::<Vec<&str>>();
+        let min = span[0].parse::<u32>().expect("Failed to parse min");
+        let max = span[1].parse::<u32>().expect("Failed to parse max");
+        let letter = parts[1].parse::<char>().expect("Failed to parse char");
+
+        Policy {
+            min,
+            max,
+            letter
+        }
+    }
+}
+
+pub struct Password {
     policy: Policy,
     password: String,
 }
 
-pub fn parse(input_name: &str) -> () {
-    todo!(fmt!("Implement parsing of input {}", input_name))
+impl Password {
+    fn parse(input: &str) -> Password {
+        let parts = input.split(":").collect::<Vec<&str>>();
+        let policy = Policy::parse(parts[0]);
+        let password = parts[1].trim().to_string();
+
+        Password {
+            policy,
+            password,
+        }
+    }
+
+    fn is_valid(&self) -> bool {
+        let occurances = self.password
+            .chars()
+            .filter(|c| c.eq(&self.policy.letter))
+            .collect::<Vec<char>>()
+            .len() as u32;
+        self.policy.min <= occurances && occurances <= self.policy.max
+    }
+
+    fn is_valid_2(&self) -> bool {
+        let chars = self.password.chars();
+        let a = chars.clone().nth((self.policy.min - 1) as usize).expect("No char at min");
+        let b = chars.clone().nth((self.policy.max - 1) as usize).expect("No char at max");
+        (a.eq(&self.policy.letter) || b.eq(&self.policy.letter)) && a.eq(&b) == false
+    }
 }
 
-pub fn part_1(input_name: &str) -> Option<()> {
-    let data = parse(input_name);
-    todo!("Implement part 1")
+pub fn parse(input_name: &str) -> Vec<Password> {
+    input_name
+        .split("\n")
+        .filter(|v| v.is_empty() == false)
+        .map(|val| Password::parse(val))
+        .collect()
 }
 
-pub fn part_2(input_name: &str) -> Option<()> {
+pub fn part_1(input_name: &str) -> u32 {
     let data = parse(input_name);
-    todo!("Implement part 2")
+    data
+        .into_iter()
+        .filter(|p| p.is_valid())
+        .collect::<Vec<Password>>()
+        .len() as u32
+}
+
+pub fn part_2(input_name: &str) -> u32 {
+    let data = parse(input_name);
+    data
+        .into_iter()
+        .filter(|p| p.is_valid_2())
+        .collect::<Vec<Password>>()
+        .len() as u32
 }
 
 
@@ -33,15 +90,7 @@ pub fn run(input: AOCInput) -> () {
     };
 
     let part_one = part_1(data);
-    if let Some(val) = part_one {
-        println!("Solution to part 1: {}", val);
-    } else {
-        println!("Found no solution to part 1 :(");
-    }
+    println!("Solution to part one: {}", part_one);
     let part_two = part_2(data);
-    if let Some(val) = part_two {
-        println!("Solution to part 2: {}", val);
-    } else {
-        println!("Found no solution to part 2 :(");
-    }
+    println!("Solution to part two: {}", part_two);
 }
