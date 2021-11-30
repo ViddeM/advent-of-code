3a336a6c364c025d5727142e68fce906297dbdf6
Vidar Magnusson <git@vidarmagnusson.com>
2021-11-30 17:29:34 +0100

    Add base for 2021

---
+++ 2021/src/days/day_0/mod.rs
@@ -0,0 +1,19 @@
+fn parse(input: &str) -> () {
+
+}
+
+pub struct Day0 {}
+
+impl Day for Day0 {
+    fn parse_bench(&self, input: &str) -> () {
+        parse(input);
+    }
+
+    fn part_1(&self, input: &str) -> String {
+        "".to_string()
+    }
+
+    fn part_2(&self, input: &str) -> String {
+        "".to_string()
+    }
+}
