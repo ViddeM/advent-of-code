7eaa8931479ba6194c8fd77746ae256515a9093d
Vidar Magnusson <git@vidarmagnusson.com>
2021-12-01 09:35:56 +0100

    More optimizations

--- 2021/src/days/day_1/mod.rs
+++ 2021/src/days/day_1/mod.rs
@@ -28,15 +28,12 @@ impl Day for Day1 {
 
     fn part_2(&self, input: &str) -> String {
         let nums = parse(input).collect::<Vec<u32>>();
-        let mut prev_sum = 0;
         let mut increased = 0;
         for (i, num) in nums.iter().enumerate() {
             if i >= 2 {
-                let new_sum = num + nums[i - 1] + nums[i - 2];
-                if prev_sum > 0 && new_sum > prev_sum {
+                if nums[i] > nums[i - 2] {
                     increased += 1;
                 }
-                prev_sum = new_sum;
             }
         }
         increased.to_string()
